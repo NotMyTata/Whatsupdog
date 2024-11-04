7e554af9be66f70f20ac48576f862ce08c2ce27d
NotMyTata <161783398+NotMyTata@users.noreply.github.com>
2024-11-04 20:50:47 +0700

    Updated UI & Navigation tabs

--- app/src/main/java/com/example/whatsupdog/MainActivity.java
+++ app/src/main/java/com/example/whatsupdog/MainActivity.java
@@ -1,14 +1,34 @@
 package com.example.whatsupdog;
 
+import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
 import android.os.Bundle;
+import android.os.Handler;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
+
+import com.google.android.material.tabs.TabLayout;
 
 public class MainActivity extends AppCompatActivity {
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
+    private NavAccessorAdapter navAccessorAdapter;
+
+    private SQLiteDatabase db;
+    private SQLiteOpenHelper dbopen;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        viewPager = (ViewPager) findViewById(R.id.viewpager);
+        navAccessorAdapter = new NavAccessorAdapter(getSupportFragmentManager());
+        viewPager.setAdapter(navAccessorAdapter);
+
+        tabLayout = (TabLayout) findViewById(R.id.tabs);
+        tabLayout.setupWithViewPager(viewPager);
     }
 }
