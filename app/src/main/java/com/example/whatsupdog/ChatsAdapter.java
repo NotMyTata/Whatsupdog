65e5bcb20059efad4251f7ebdefdc017b7bd3674
NotMyTata <161783398+NotMyTata@users.noreply.github.com>
2024-11-05 08:39:08 +0700

    Fixed Chat Adapter & Layout

--- app/src/main/java/com/example/whatsupdog/ChatsAdapter.java
+++ app/src/main/java/com/example/whatsupdog/ChatsAdapter.java
@@ -6,6 +6,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import org.w3c.dom.Text;
@@ -13,11 +14,11 @@ import org.w3c.dom.Text;
 import java.util.ArrayList;
 
 public class ChatsAdapter extends ArrayAdapter<Chat> {
-    // View lookup cache
+
     private static class ViewHolder {
-        int profile;
+        ImageView profile;
         TextView name;
-        int status;
+        ImageView status;
         TextView content;
         TextView time;
         TextView unread;
@@ -29,32 +30,31 @@ public class ChatsAdapter extends ArrayAdapter<Chat> {
 
     public View getView(int position, View ConvertView, ViewGroup parent) {
         Chat data = getItem(position);
-        // Check if an existing view is being reused, otherwise inflate the view
-        ViewHolder viewChats; // view lookup cache stored in tag
+
+        ViewHolder viewChats;
         if (ConvertView == null) {
-            // If there's no view to re-use, inflate a brand new view for row
             viewChats = new ViewHolder();
-            ConvertView = LayoutInflater.from(getContext()).inflate(R.layout.fragment_chats, parent, false);
-            viewChats.profile = (int) ConvertView.findViewById(R.id.chat_profile_image);
+            ConvertView = LayoutInflater.from(getContext()).inflate(R.layout.chats_row, parent, false);
+
+            viewChats.profile = (ImageView) ConvertView.findViewById(R.id.chat_profile_image);
             viewChats.name = (TextView) ConvertView.findViewById(R.id.chat_name);
+            viewChats.status = (ImageView) ConvertView.findViewById(R.id.chat_status);
             viewChats.content = (TextView) ConvertView.findViewById(R.id.chat_content);
             viewChats.time = (TextView) ConvertView.findViewById(R.id.chat_time);
-            viewChats.unread = (TextView) ConvertView.findViewById(R.id.chat_unread);
-            // Cache the viewHolder object inside the fresh view
-            ConvertView.setTag(viewChats);
+            viewChats.unread = (TextView) ConvertView.findViewById(R.id.chat_unread_text);
 
-            Button btn = (Button) ConvertView.findViewById(R.id.btn);
-            btn.setTag(position);
-            btn.setOnClickListener(op);
+            ConvertView.setTag(viewChats);
         } else {
             viewChats = (ViewHolder) ConvertView.getTag();
         }
-        viewChats.profile
-        viewChats.name.setText(data.getName());
 
+        viewChats.profile.setImageResource(data.getProfile());
+        viewChats.name.setText(data.getName());
+        viewChats.status.setImageResource(data.getStatus());
         viewChats.content.setText(data.getContent());
         viewChats.name.setText(data.getName());
         viewChats.content.setText(data.getContent());
+
         return ConvertView;
     }
 }
--- app/src/main/java/com/example/whatsupdog/ChatsFragment.java
+++ app/src/main/java/com/example/whatsupdog/ChatsFragment.java
@@ -13,33 +13,41 @@ import android.widget.ListView;
 import java.util.ArrayList;
 
 public class ChatsFragment extends Fragment {
-    private ArrayAdapter<String> arrayAdapter;
-    private ArrayList<String> arrayList;
+    private ChatsAdapter chatsAdapter;
+    private ArrayList<Chat> chatList;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View groupFragmentView = inflater.inflate(R.layout.fragment_chats, container, false);
         initialization(groupFragmentView);
-        // Inflate the layout for this fragment
         return groupFragmentView;
     }
 
     private void initialization(View v) {
         ListView lv = v.findViewById(R.id.listview);
-        arrayAdapter = new ArrayAdapter<String>(getContext(), R.layout.fragment_chats, arrayList);
+        chatList = new ArrayList<Chat>();
+        chatsAdapter = new ChatsAdapter(getContext(), R.layout.fragment_chats, chatList);
+
         int[] profile = {R.drawable.baseline_person_24_blue, R.drawable.baseline_person_24_red};
         String[] name = {"Test", "Arad"};
-        int[] status = {3, 2};
+        int[] status = {R.drawable.baseline_done_all_18_read, R.drawable.baseline_done_all_18_received};
         String[] content = {"Hello World", "Bucharest"};
         String[] time = {"21:30", "07:00"};
         String[] unread = {"0", "1"};
         addAll(profile, name, status, content, time, unread);
+
+        lv.setAdapter(chatsAdapter);
     }
 
     private void addAll(int[] profile, String[] name, int[] status, String[] content, String[] time, String[] unread) {
         for (int i = 0; i < profile.length; i++) {
-            arrayList.
+            addChat(profile[i], name[i], status[i], content[i], time[i], unread[i]);
         }
+        chatsAdapter.notifyDataSetChanged();
+    }
+
+    private void addChat(int profile, String name, int status, String content, String time, String unread) {
+        chatList.add(new Chat(profile, name, status, content, time, unread));
     }
 }
